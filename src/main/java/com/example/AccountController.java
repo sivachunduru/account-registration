8e4f31a64d577ef2253217fc21ce43daa3b5d57a
Siva Rama Krishna Chunduru <sivachunduru@gmail.com>
2019-05-28 21:35:12 +0530

    Update AccountController.java

--- src/main/java/com/example/AccountController.java
+++ src/main/java/com/example/AccountController.java
@@ -210,6 +210,7 @@ public class AccountController {
 
 	@Bean
 	public DataSource getDataSource() throws SQLException, URISyntaxException {
+		System.out.println("dbUrl: "+dbUrl);
 		if (dataSource == null) {
 			if (dbUrl == null || dbUrl.isEmpty()) {
 				dataSource = new HikariDataSource();
